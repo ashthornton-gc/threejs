88eebe88924cffc31c7965e887158455653b0aa3
Ash Thornton <ash@craftedbygc.com>
2018-02-28 08:59:57 +0000

    project 5

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 //import loader from './projects/loader';
 // import depthOfField from './projects/depth-of-field';
 // import project3 from './projects/project3';
-import project4 from './projects/project4';
+//import project4 from './projects/project4';
+import project5 from './projects/project5';
 
 //loader();
 // depthOfField();
 // project3();
-project4();
+//project4();
+project5();
--- js/projects/project4.js
+++ js/projects/project4.js
@@ -15,6 +15,7 @@ const project4 = function() {
 
         renderer = new THREE.WebGLRenderer({antialias: false});
         renderer.setSize(renderWidth, renderHeight);
+        renderer.shadowMap.enabled = true;
         document.body.appendChild(renderer.domElement);
 
         scene = new THREE.Scene();
@@ -23,47 +24,81 @@ const project4 = function() {
 
         controls = new OrbitControls( camera );
 
-        const gridHelper = new THREE.GridHelper(20, 20);
-        gridHelper.position.set(0,0,10);
-        scene.add( gridHelper );
+        //const gridHelper = new THREE.GridHelper(20, 20);
+        //gridHelper.position.set(0,0,0);
+        //scene.add( gridHelper );
 
-        camera.position.set(25, 15, 35);
-        camera.lookAt(new THREE.Vector3(0, 0, 20));
+        camera.position.set(45, 15, 35);
+        camera.lookAt(new THREE.Vector3(0));
 
-        let light = new THREE.PointLight(0x5FD6E3, 0.8, 150, 0.5);
+        let light = new THREE.PointLight(0x5FD6E3, 0.8, 50, 0.5);
         light.castShadow = true;
-        light.position.set(4, 20, 23); // turq
+        light.position.set(0, 20, 25); // turq
         scene.add( light );
 
         scene.add( new THREE.PointLightHelper( light ) );
 
-        light = new THREE.PointLight(0xDBB1F0, 0.8, 150, 0.5);
+        light = new THREE.PointLight(0xDBB1F0, 0.8, 50, 0.5);
         light.castShadow = true;
-        light.position.set(8, 20, 20); //pink
+        light.position.set(0, 20, 0); //pink
         scene.add( light );
 
         scene.add( new THREE.PointLightHelper( light ) );
 
-        light = new THREE.PointLight(0x7554D9, 0.8, 150, 0.5);
+        light = new THREE.PointLight(0x7554D9, 0.8, 50, 0.5);
         light.castShadow = true;
-        light.position.set(13, 20, 20); //purple
+        light.position.set(0, 20, -25); //purple
         scene.add( light );
 
         scene.add( new THREE.PointLightHelper( light ) );
 
         scene.add( new THREE.HemisphereLight(0xFFFFFF, 0x000000, 0.5));
 
-        planeGeometry = new THREE.PlaneGeometry(20, 10, 20);
-        planeMaterial = new THREE.MeshLambertMaterial( { color: 0xCCCCCC, side: THREE.DoubleSide } );
+        let group = new THREE.Group();
+
+        let length = 0.15, width = 25;
+
+        let shape = new THREE.Shape();
+        shape.moveTo( 0,0 );
+        shape.lineTo( 0, width );
+        shape.lineTo( length, width );
+        shape.lineTo( length, 0 );
+        shape.lineTo( 0, 0 );
+
+        let extrudeSettings = {
+            steps: 24,
+            amount: 24,
+            bevelEnabled: false,
+            bevelThickness: 0.05,
+            bevelSize: 0.1,
+            bevelSegments: 20,
+            curveSegments: 20
+        };
+
+        planeGeometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+
+        //planeGeometry = new THREE.PlaneGeometry(20, 10, 20);
+        planeMaterial = new THREE.MeshLambertMaterial( {
+            color: 0xFFFFFF,
+            //side: THREE.DoubleSide,
+            wireframe: false,
+            transparent: true,
+            opacity: 0.9
+        } );
 
         for( let i = 0; i < 20; i++ ) {
 
             planes[i] = new THREE.Mesh( planeGeometry, planeMaterial );
-            planes[i].castShadow = true;
-            planes[i].position.set( 0, 0, i + 1 );
-            scene.add( planes[i] );
+            //planes[i].castShadow = true;
+            planes[i].receiveShadow = true;
+            planes[i].position.set( i * 1.5, 0, 0 );
+            group.add( planes[i] );
 
         }
+
+        new THREE.Box3().setFromObject( group ).getCenter( group.position ).multiplyScalar( - 1 );
+
+        scene.add( group );
 
         window.addEventListener('resize', onWindowResize, false);
 
@@ -92,7 +127,7 @@ const project4 = function() {
 
         timeElapsed = clock.getElapsedTime();
 
-        let center = new THREE.Vector2(20, 0);
+        let center = new THREE.Vector2(0, 0);
 
         for ( let x = 0; x < planes.length; x++ ) {
 
@@ -101,9 +136,9 @@ const project4 = function() {
             for (let i = 0; i < vLength; i++) {
                 let v = planes[x].geometry.vertices[i];
                 let dist = new THREE.Vector2(v.x, v.y).sub(center);
-                let size = 5.0;
+                let size = 10;
                 let magnitude = 4;
-                v.z = Math.sin(dist.length()/-size + (timeElapsed * 0.5)) * magnitude;
+                v.x = Math.sin(dist.length()/-size + (timeElapsed * 0.5)) * magnitude;
             }
             planes[x].geometry.verticesNeedUpdate = true;
 
